084548f0486d0aa7414ebb44261fe23765228106
David Nguyen <davidnguyen234@gmail.com>
2019-07-16 15:07:31 -0700

    hw2
    complete

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const http = require("http"); 
 
-  const callback = (req,res) => {
+let albums = require("./lib/data.js");
+let http = require("http"); 
 
-  const path = req.url.toLowerCase();
+
+http.createServer((req,res) => {
+  const querystring = require('querystring');
+  let url = req.url.toLowerCase().split("?");
+  let fs = require("fs");
+  let queryParams = querystring.parse(url[1]);
+  let path = url[0].toLowerCase();
+    
   switch(path) {
     case '/':
-    const fs = require("fs");
-    fs.readFile("/Users/davidnguyen/Desktop/itc230/itc230/public/home.html", (err, data) => {
+    fs.readFile("public/home.html", (err, data) => {
       if (err) return console.error(err);
       res.writeHead(200, {'Content-Type': 'text/html'});
       res.end(data.toString());
@@ -16,12 +22,34 @@ const http = require("http");
       res.writeHead(200, {'Content-Type': 'text/plain'});
       res.end('About page');
       break;
+
+      case '/detail':
+      let foundAlbum = albums.getItem(queryParams.title); 
+      let getMessage = (foundAlbum) ? JSON.stringify(foundAlbum) : "Not found";
+      res.writeHead(200, { 'Content-Type': 'text/plain' } );
+      res.end('Results for ' + queryParams.title + ":\n\n" + getMessage);
+    break;
+
+      case '/delete':
+      if(url[1] ==! undefined){
+
+        fs.readFile("public/error.html", (err, data) => {
+            if (err) return console.error(err);
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(data.toString());
+        });
+      } else {
+        let deleteParams = url[1].split("=");
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end(JSON.stringify(albums.getItem(deleteParams[1]))  + ' has been deleted');
+      albums.deleteItem(deleteParams[1]);
+      }
+      console.log(albums.getAll())
+
+      break;
     default:
       res.writeHead(404, {'Content-Type': 'text/plain'});
       res.end('Not found');
-      break;
-    }
   }
-
-  http.createServer(callback).listen(process.env.PORT || 3000);
-
+  
+}).listen(process.env.PORT || 3000);
