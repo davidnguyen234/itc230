aef7502cd612d8dd177a29d116df2c2d12cbb858
David Nguyen <davidnguyen234@gmail.com>
2019-07-25 13:10:00 -0700

    hw3
    complete

--- index.js
+++ index.js
@@ -1,3 +1,65 @@
+'use strict'
+const albums = require("./lib/data");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+app.use(express.static(__dirname + '/public')); // set location for static files
+app.use(bodyParser.urlencoded({extended: true})); // parse form submissions
+
+const handlebars = require("express-handlebars");
+app.engine(".html", handlebars({extname: '.html', defaultLayout: false}));
+app.set("view engine", ".html");
+
+/* 
+
+send static file as response
+app.get('/', (req, res) => {
+    res.type('text/html');
+    res.sendFile(__dirname + '/public/home.html'); 
+   });
+*/
+
+// send content of 'home' view
+app.get('/', (req, res) => {
+  res.render('home',{name: req.query.name});
+});
+
+// send plain text response
+app.get('/about', (req, res) => {
+  res.type('text/html');
+  res.sendFile(__dirname + '/public/about.html');
+});
+
+
+app.post('/detail', (req,res) => {
+  let result = albums.getItem(req.body.album);
+  res.render('detail', {title: req.body.album, result: result})
+});
+
+
+app.get('/delete', (req,res) => {
+  let result = albums.deleteItem(req.query.album); 
+  res.render('delete', {deletedItem: req.query.album, result: result});
+});
+
+
+
+// define 404 handler
+app.use((req,res) => {
+  res.type('text/plain');
+  res.status(404);
+  res.send('404 - Not found');
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Express started');
+});
+
+
+
+/*
 
 let albums = require("./lib/data.js");
 let http = require("http"); 
@@ -53,3 +115,5 @@ http.createServer((req,res) => {
   }
   
 }).listen(process.env.PORT || 3000);
+
+*/
